907d7c3d9fdbea8f1ed7fd1218d17b06fdde4c5b
Ivan Kalinin <pupssman@gmail.com>
2014-04-11 01:25:24 +0400

    A launchable dummy server

--- vmtp_mud/server.py
+++ vmtp_mud/server.py
@@ -6,6 +6,34 @@ It contains a core entry point ``main`` that is used to run server -- it blocks 
 @author: pupssman
 """
 
+import xmlrpc.server
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
+class GameServer:
+    """
+    Implements actual MUD server
+    """
+
 
 def main():
-    print("Hello, world -- I am MUD server!")
+    host = '127.0.0.1'
+    port = 8081
+
+    server = xmlrpc.server.SimpleXMLRPCServer((host, port))
+
+    server.register_instance(GameServer())
+
+    logging.info("MUD serving at enpoint http://{0}:{1}/".format(host, port))
+    logging.info("Press Ctrl-C to stop")
+
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        logging.info("Stopped")
+
+
+if __name__ == '__main__':
+    main()
